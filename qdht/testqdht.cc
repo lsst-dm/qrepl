1aa3ae8d840012a520895149b524e3033ea04939
Fritz Mueller <fritzm@slac.stanford.edu>
2015-12-11 14:19:20 -0800

    Initial project skeleton
    Overall eups/CMake/Eclipse project skeleton, including an empty
    qdht static library with capnproto tooling, and (empty) associated
    unit tests.

---
+++ qdht/testqdht.cc
@@ -0,0 +1,10 @@
+#define BOOST_TEST_MODULE qdht
+#include "boost/test/included/unit_test.hpp"
+
+using namespace std;
+
+namespace lsst {
+namespace qserv {
+namespace qdht {
+
+}}} // namespace lsst::qserv::qdht
